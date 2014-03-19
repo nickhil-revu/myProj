14bcc0ee9b63cfd25ed7a6f1eb0d81a3c46956fb
Nickhil Revu <nickhil_r@yahoo.com>
2014-03-19 16:55:16 -0500

    Nickhil

---
+++ RunningAssistant/src/com/group6/runningassistant/StepListener.java
@@ -0,0 +1,13 @@
+package com.group6.runningassistant;
+
+
+/**
+ * Interface implemented by classes that can handle notifications about steps.
+ * These classes can be passed to StepDetector.
+ * @author Levente Bagi
+ */
+public interface StepListener {
+    public void onStep();
+    public void passValue();
+}
+
